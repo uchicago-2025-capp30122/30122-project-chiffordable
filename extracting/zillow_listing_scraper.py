04c6767516855ee761b3c831fb7d83e7fad3d7aa
Daniela Ayala <danayala@uchicago.edu>
2025-02-23 02:19:42 -0600

    listings map

--- extracting/zillow_listing_scraper.py
+++ extracting/zillow_listing_scraper.py
@@ -1,20 +1,63 @@
 import time
+import httpx
+import lxml.html
+import json
 import csv
-import re
+import os
 import pandas as pd
-from zillow_utils import fetch_page, parse_script_content, save_listings_to_csv
+from zillow_utils import parse_script_content, save_listings_to_csv
+
+headers = {
+    "method": "GET",
+    "scheme": "https",
+    "authority": "www.zillow.com",
+    "path": "/apartments/elk-grove-village-il/willow-crossing-apartments/5Xt94L/",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Cookie": "sessionid=abc123",
+    "Host": "www.zillow.com",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15"
+}
+
+
+
+def fetch_page(url: str) -> str:
+    """
+    Fetches the content of a webpage given a URL using httpx.
+    """
+
+    try:
+        with httpx.Client(headers=headers, follow_redirects=True) as client:
+            response = client.get(url)
+            
+            if response.status_code == 403:
+                return None  # Return None if forbidden
+            
+            response.raise_for_status()  # Raise an exception for other HTTP errors
+            return response
+    except httpx.HTTPStatusError:
+        return None  # Return None if any other HTTP error occurs
+    except httpx.RequestError:
+        return None  # Return None if there are connection issues
 
 
 def extract_listings(json_data: dict):
     """
     Extracts the list of property listings from the parsed JSON data.
     """
-    listings = json_data['props']['pageProps']['componentProps']['initialReduxState']['gdp']['building']['floorPlans']
-    
-    if listings is None:
-        listings = json_data['props']['pageProps']['componentProps']['initialReduxState']['gdp']['building']['ungroupedUnits']
-    
-    return listings
+    try:
+        partial_extract = json_data['props']['pageProps']['componentProps']['initialReduxState']['gdp']['building']
+        listings = partial_extract.get('floorPlans') 
+        if not listings:
+            listings= partial_extract.get('ungroupedUnits')
+        return listings
+    except KeyError:
+        return []
 
 
 def get_listing_info(listings:list):
@@ -59,16 +102,19 @@ def get_listing_info(listings:list):
             })
     return results
 
-def get_prices(url)-> list:
-    detail_link = str(url)
-    
-    html = fetch_page(detail_link)
-    json_data = parse_script_content(html)
-    if json_data:
-        listings = extract_listings(json_data)
-        # print("Fetching URl:", url)
 
-    return get_listing_info(listings)
+
+def get_prices(url)-> list:
+     detail_link = str(url)
+     html = fetch_page(detail_link)
+     if html:
+        json_data = parse_script_content(html.text)
+        if json_data:
+            listings = extract_listings(json_data)
+            print("Fetching URl:", url)
+            if listings:
+                info = get_listing_info(listings)
+                return info
 
 
 def get_missing_listings():
@@ -82,35 +128,37 @@ def get_missing_listings():
             if not row['price']:
                 url = row['detailUrl']
                 if url not in fetched_links:
-                    time.sleep(0.1)
+                    time.sleep(0.01)
                     individual_info = get_prices(url)
+                    if individual_info:
+                        for appartment in individual_info:
+                            apparment_data = {
+                                "address": row['address'],
+                                "detailUrl": row['detailUrl'],
+                                "statusType": row['statusType'],
+                                "zipcode": row['zipcode'],
+                                "latitude": row['latitude'],
+                                "longitude": row['longitude'],
+                                "price": appartment['price'],
+                                "clean_price": appartment['price'],
+                                "livingarea": appartment['sqft'],
+                                "status": row['status'],
+                                "listingkey": row['listingkey'],
+                                "bedrooms": appartment['beds'], 
+                                "bathrooms": appartment['baths'] ,
+                                }
+                            
+                            completed_data.append(apparment_data)
 
-                    for appartment in individual_info:
-                        apparment_data = {
-                            "address": row['address'],
-                            "detailUrl": row['detailUrl'],
-                            "statusType": row['statusType'],
-                            "zipcode": row['zipcode'],
-                            "latitude": row['latitude'],
-                            "longitude": row['longitude'],
-                            "price": appartment['price'],
-                            "clean_price":re.sub(r"[^\d]", "", str(appartment['price'])) if appartment['price'] else None,
-                            "livingarea": appartment['sqft'],
-                            "status": row['status'],
-                            "listingkey": row['listingkey'],
-                            "bedrooms": appartment['beds'], 
-                            "bathrooms": appartment['baths'] ,
-                            }
-                        
-                        completed_data.append(apparment_data)
-
-                        fetched_links.add(url)
+                            fetched_links.add(url)
 
             else:
                 completed_data.append(row)
     
     # Save the cleaned data to CSV
     save_listings_to_csv(completed_data, "Zillow-complete.csv")
+
+get_missing_listings()
 
 
 
--- extracting/zillow_utils.py
+++ extracting/zillow_utils.py
@@ -21,28 +21,25 @@ def complete_link(url: str) -> str:
     
     return base_url + url
 
-default_headers = { 
-        "authority": "www.zillow.com",
-        "method": "GET",
-        "scheme": "https",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-encoding": "gzip, deflate, br, zstd",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "max-age=0",
-        "cookie": "zguid=24|%24a54d78f6-9268-4405-9be6-3f3663abd80e",  # Replace with your actual cookies if needed
-        "priority": "u=0, i",
-        "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"macOS"',
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
-        }
+headers = {
+    "method": "GET",
+    "scheme": "https",
+    "authority": "www.zillow.com",
+    "path": "/apartments/elk-grove-village-il/willow-crossing-apartments/5Xt94L/",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Cookie": "sessionid=abc123",
+    "Host": "www.zillow.com",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15"
+}
 
-def fetch_page(url: str, headers= default_headers) -> str:
+
+def fetch_page(url: str) -> str:
     """
     Fetches the content of a webpage given a URL using httpx.
     """
@@ -51,6 +48,7 @@ def fetch_page(url: str, headers= default_headers) -> str:
         response = client.get(url)
         response.raise_for_status()  # Raise an exception for HTTP errors
         return response.text
+    
     
 def parse_script_content(html: str):
     """
