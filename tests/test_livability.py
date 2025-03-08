c272ccbbf984d68e85e2d918a118487dd045adc1
Maria Jose Reyes Ramirez <mariajosereyesramirez@Marias-MacBook-Pro.local>
2025-03-07 22:08:48 -0600

    still failing one test

--- tests/test_livability.py
+++ tests/test_livability.py
@@ -6,9 +6,9 @@ import regex  as re
 import csv
 import os
 import json
-import unittest.mock import patch
+from unittest.mock  import patch
 from pathlib import Path
-from extracting.livability import make_table_request, livindex_by_zc
+from extracting.livability import make_table_request, livindex_by_zc, extract_next_chars
 
 
 # Path for functions 
@@ -30,21 +30,26 @@ def mock_response():
         "score_opp": 85,
         "score_trans": 60
     }
-@patch("extracting.livability.httpx.Client.get")
 
-def test_make_table_request_success(mock_get, mock_response):
+@patch("extracting.livability.extract_next_chars")
+@patch("httpx.Client.get")
+def test_make_table_request_success(mock_extract_next_chars, mock_get, mock_response):
     """
     Testing function make_table_request is returning data 
     """
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.json.return_value = mock_response
-# Sample zipcodes for tests
+
+    mock_http_response = httpx.Response(200, json=mock_response)
+    mock_get.return_value = mock_http_response  
+
+    mock_extract_next_chars.return_value = mock_response  
+
+    # Sample zipcodes for tests
     mock_zip_code = "60601"
     result = make_table_request(mock_zip_code)
 
     assert result == mock_response, f"Expected{mock_response}, but got {result}"
 
-@patch("extracting.libavility.httpx.make_table_request")
+@patch("extracting.livability.make_table_request")
 def test_livindex_by_zc_valid(mock_make_table_request):
     """
     Testing valid zipcodes
@@ -52,27 +57,27 @@ def test_livindex_by_zc_valid(mock_make_table_request):
 
     mock_make_table_request.side_effect = [
         {"score_health": 90, "score_env": 85},
-     {"score_health": 80, "score_env": 75}
-     ]
+        {"score_health": 80, "score_env": 75}
+    ]
     
     zip_codes = ["60601", "60602"]
     result = livindex_by_zc(zip_codes)
 
     expected = [
         {"score_health": 90, "score_env": 85, "zip_code": "60601"},
-        {"score_health":80, "score_env": 75, "zip_code": "60602"}
+        {"score_health": 80, "score_env": 75, "zip_code": "60602"}
     ]
     assert result == expected, f"Expected{expected}, but obtained {result}"
 
-@patch("extracting.libavility.httpx.make_table_request")
+@patch("extracting.livability.make_table_request")
 def test_livindex_by_zc_empty_list(mock_make_table_request):
     result = livindex_by_zc([])
     assert result == [], "was expecting an empty list"
 
-@patch("extracting.libavility.httpx.make_table_request")
+@patch("extracting.livability.make_table_request")
 def test_livindex_by_zc_invalid_zip(mock_make_table_request):
 
-    make_table_request.sideeffect = [None]
+    mock_make_table_request.return_value = None
 
     zip_codes = ["99999"]
     result = livindex_by_zc(zip_codes)
