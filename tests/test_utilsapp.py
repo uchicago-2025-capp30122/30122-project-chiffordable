3d2e0e4e2959f90dbdcae79d96985bbd614fc7e1
Manuel Cardona <mcardonaarias96@gmail.com>
2025-03-09 15:18:00 -0500

    Tests community_point

--- tests/test_utilsapp.py
+++ tests/test_utilsapp.py
@@ -3,26 +3,42 @@ from app.Utils_app import gdf_to_geojson, get_community_from_point, calculate_re
 from shapely import wkt
 import geopandas as gpd
 
+df_communities_test = pd.read_csv("extracted_data/cmap.csv") # Importing external data
+df_communities_test = df_communities_test[["GEOG", "comm_poly"]]
+df_communities_test['geometry'] = df_communities_test['comm_poly'].apply(lambda x: wkt.loads(x) if isinstance(x, str) else x)
+df_communities_test = gpd.GeoDataFrame(df_communities_test, geometry='geometry')
 
-
-
-def test_community_point():
-    df_communities_test = pd.read_csv("extracted_data/cmap.csv") # Importing external data
-    df_communities_test = df_communities_test[["GEOG", "comm_poly"]]
-    df_communities_test['geometry'] = df_communities_test['comm_poly'].apply(lambda x: wkt.loads(x) if isinstance(x, str) else x)
-    df_communities_test = gpd.GeoDataFrame(df_communities_test, geometry='geometry')
-
+def test_community_point_normalset():
     coords = [(str(41.883717), str(-87.62866)),
-          (str(41.886776), str(-87.624725)),
+          (str(41.71056), str(-87.53684)),
           (str(41.886917), str(-87.615364)),
-          (str(41.887234), str(-87.61864)),
-          (str(41.8865), str(-87.61972))]
+          (str(41.918922), str(-87.718216)),
+          (str(41.774254), str(-87.60619))]
+    communities_correct = ["The Loop", "East Side", "The Loop", 
+                           "Logan Square", "Woodlawn"]
+    
+    communities = []
+    for pair in coords:
+        latitude = pair[0]
+        longitude = pair[1]
+        community = get_community_from_point(df_communities_test, latitude, longitude)["GEOG"]
+        communities.append(community)
+
+    assert communities == communities_correct
+
+def test_community_point_outside():
+    coords = [(str(21.870553844846082), str(-102.29575199109235))]
+    communities_correct = [None]
     
     communities = []
     for pair in coords:
         latitude = pair[0]
         longitude = pair[1]
         community = get_community_from_point(df_communities_test, latitude, longitude)
-        print(community)
-        communities.append(community)
-    assert communities == []
+        if community:
+            community_name = community["GEOG"]
+        else:
+            community_name = None
+        communities.append(community_name)
+
+    assert communities == communities_correct
