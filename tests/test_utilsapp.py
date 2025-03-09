5e9ded3ba897d8ad5714ccac0b6ce68a1e103a1f
Manuel Cardona <mcardonaarias96@gmail.com>
2025-03-09 15:34:32 -0500

    Finished merge tests

--- tests/test_utilsapp.py
+++ tests/test_utilsapp.py
@@ -1,10 +1,9 @@
 import pandas as pd
-from app.Utils_app import gdf_to_geojson, get_community_from_point, calculate_rent, get_community_from_name, get_livability_scores
+from app.Utils_app import get_community_from_point, calculate_rent, get_community_from_name
 from shapely import wkt
 import geopandas as gpd
 
 df_communities_test = pd.read_csv("extracted_data/cmap.csv") # Importing external data
-df_communities_test = df_communities_test[["GEOG", "comm_poly"]]
 df_communities_test['geometry'] = df_communities_test['comm_poly'].apply(lambda x: wkt.loads(x) if isinstance(x, str) else x)
 df_communities_test = gpd.GeoDataFrame(df_communities_test, geometry='geometry')
 
@@ -42,3 +41,68 @@ def test_community_point_outside():
         communities.append(community_name)
 
     assert communities == communities_correct
+
+def test_community_name_normalset():
+    community_names = ["The Loop", "East Side", "Woodlawn"]
+    correct_details = [
+        (41671.0, 2.6, 11.5),
+        (23869.723396934627, 7.0, 86.8),
+        (23865.0, 4.6, 3.1)
+    ]
+    details = []
+    for community in community_names:
+        community = get_community_from_name(df_communities_test, community)
+        details_community = (community["TOT_POP"], community["UND5"], community["HISP"])
+        details.append(details_community)
+
+    assert details == correct_details
+
+def test_community_name_unknown():
+    community_names = ["Aguascalientes"]
+    correct_details = [
+        None
+    ]
+    details = []
+    for community in community_names:
+        community = get_community_from_name(df_communities_test, community)
+        if community:
+            details_community = (community["TOT_POP"], community["UND5"], community["HISP"])
+        else:
+            details_community = None
+        details.append(details_community)
+
+    assert details == correct_details
+
+def test_calculate_rent_normalset():
+    incomes = [100000, 30000, 72000]
+    shares = [10, 33.3, 41]
+    correct_maxrent = [833.3333333333334, 2774.9999999999995, 3416.6666666666665,
+                       250.0, 832.4999999999999, 1025.0,
+                       600.0, 1998.0, 2460.0]
+    rents = []
+    for income in incomes:
+        for share in shares:
+            rents.append(calculate_rent(income, share))
+    assert rents  == correct_maxrent
+
+def test_calculate_rent_noincome():
+    incomes = [0, 0, 0]
+    shares = [10, 33.3, 41]
+    correct_maxrent = [0, 0, 0,
+                       0, 0, 0,
+                       0, 0, 0]
+    rents = []
+    for income in incomes:
+        for share in shares:
+            rents.append(calculate_rent(income, share))
+    assert rents  == correct_maxrent
+
+def test_calculate_rent_noshare():
+    incomes = [100000, 30000, 72000]
+    shares = [None]
+    correct_maxrent = incomes
+    rents = []
+    for income in incomes:
+        for share in shares:
+            rents.append(calculate_rent(income, share))
+    assert rents  == correct_maxrent
